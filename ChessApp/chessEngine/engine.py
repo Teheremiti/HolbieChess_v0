65dafc802662c8d10ac13a7f582b8c95536ef298
Irrisam <124586557+Irrisam@users.noreply.github.com>
2023-10-19 09:56:30 +0000

    re add files after solving moving conflicts

--- ChessApp/chessEngine/engine.py
+++ ChessApp/chessEngine/engine.py
@@ -7,12 +7,88 @@ class IA:
     # init IA class
     def __init__(self):
         self.engine = chess.engine.SimpleEngine.popen_uci(stockfish_path)
+        self.pawn = chess.PieceType(1)
+        self.knight = chess.PieceType(2)
+        self.bishop = chess.PieceType(3)
+        self.rook = chess.PieceType(4) 
+        self.queen = chess.PieceType(5)
+        self.king = chess.PieceType(6)
+        self.white = chess.WHITE
+        self.black = chess.BLACK
+        self.pawn_value = 10
+        self.knight_value = 30
+        self.bishop_value = 30
+        self.rook_value = 50
+        self.queen_value = 90
+        self.king_value = 900
 
     def make_move(self, board):
         # Move choosing function
         result = self.engine.play(board, chess.engine.Limit(time=0.1))
         move = result.move
-        return move  
+        return move
+    
+    def get_piece_value(self, piece):
+    # Assign values to piece objects
+        piece_values = {
+            self.pawn: self.pawn_value,
+            self.knight: self.knight_value,
+            self.bishop: self.bishop_value,
+            self.rook: self.rook_value,
+            self.queen: self.queen_value,
+            self.king: self.king_value,
+        }
+
+        # Assign proper valuez to pieces
+        piece_value = piece_values.get(piece.piece_type, 0)
+
+        # Turn the value negative for black
+        if piece.color == self.black:
+            piece_value = -piece_value
+
+        return piece_value
+    
+    def evaluate_board(self, board):
+        # Evaluate board function
+        # Returns the board value
+        value = 0
+        for i in range(64):
+            # Scan through all squares on the board
+            piece = board.piece_at(i)
+            if piece != None:
+                # Add value for white pieces and subtract for black pieces
+                if piece.color == self.white:
+                    value += self.get_piece_value(piece)
+                else:
+                    value -= self.get_piece_value(piece)
+        return value
+    
+    def choose_move(self, board, colour_to_play):
+        # Choose move function
+        # Returns the best move
+        best_move = None
+        best_value = -9876
+        for move in board.legal_moves:
+            # Create potential board
+            board.push(move)
+            # Evaluate the board
+            board_value = self.evaluate_board(board)
+            print(board_value)
+            # Keep value while resetting the board to original state
+            board.pop()
+            if colour_to_play == False:
+                best_value = 9876
+                if board_value < best_value:
+                # If the move is better than the best move, it becomes the new best move
+                    best_value = board_value
+                    best_move = move
+            if colour_to_play == True:
+                if board_value > best_value:
+                    # If the move is better than the best move, it becomes the new best move
+                    best_value = board_value
+                    best_move = move
+        return best_move
+        
 
 def validity_check(board):
     while True:
@@ -20,7 +96,9 @@ def validity_check(board):
             # Get human move (from command line for now) and checks for its validity
             # Also checks for wrong input
             usr_input = input("Enter your move: ")
+            # Gets san value for input type
             san_move = usr_input
+            # Turns it into uci for value check
             uci_move = board.parse_san(san_move)
             if uci_move in board.legal_moves:
                 return usr_input
@@ -35,15 +113,23 @@ board = chess.Board()
 ia = IA()
 
 try:
-    while not board.is_game_over():
+    #while not board.is_game_over():
+    for _ in range(5):
         # Loop the game until it's over
         user_move = validity_check(board)
         board.push_san(user_move)
         print(board)
         if board.is_game_over():
             break
-        move = ia.make_move(board)
+        if board.turn == chess.WHITE:
+            colour_to_play = chess.WHITE
+        else:
+            colour_to_play = False
+        #move = ia.make_move(board)
+        move = ia.choose_move(board, colour_to_play)
         board.push(move)
+        eval = ia.evaluate_board(board)
+        print (eval)
         print(board)
 except KeyboardInterrupt:
     pass
